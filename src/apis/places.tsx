9ba264a57ef8d1f80a7ee39f358a1915da70df78
seyeonk1 <seyeon.k1@gmail.com>
2022-10-02 01:29:30 +0900

    delete place api 연동

--- src/apis/places.tsx
+++ src/apis/places.tsx
@@ -29,3 +29,13 @@ export async function editPlace(data: {id: number, name: string, address:string}
         console.log(e)
     }
 }
+
+export async function deletePlace(id:string) {
+    try {
+        const res = await axios.delete(`https://api.podoting.com/admin/places/${id}`)
+        return res
+    } catch (e) {
+        console.log("장소 삭제 도중 오류 발생", e)
+        throw new Error()
+    }
+}
