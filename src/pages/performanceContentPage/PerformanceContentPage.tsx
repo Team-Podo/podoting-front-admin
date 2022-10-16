e5152ea478ed97bb398bd326f12eab2cd0c18d9d
seyeonk1 <seyeon.k1@gmail.com>
2022-10-16 23:35:27 +0900

    퍼포먼스 컨텐츠 페이지에 우측메뉴바 추가

--- src/pages/performanceContentPage/PerformanceContentPage.tsx
+++ src/pages/performanceContentPage/PerformanceContentPage.tsx
@@ -4,17 +4,19 @@ import {DetailWrapper} from "../performanceDetailPage/PerformanceDetailPage.styl
 import {useParams} from "react-router-dom";
 import '@toast-ui/editor/dist/toastui-editor.css';
 import WysiwygEditor from "../../components/editor/editor";
+import Menu from "../../components/Menu/Menu";
 
 function PerformanceContentPage() {
     const { performanceID } = useParams()
 
     return <>
         <DetailWrapper>
-            <div className="common-section">
+            <div className="info common-section">
                 <div className="wrapper">
                     <div className="info-left">
                         <WysiwygEditor performanceID={performanceID!}/>
                     </div>
+                    <Menu current={"content"} performanceID={performanceID!}/>
                 </div>
             </div>
         </DetailWrapper>
